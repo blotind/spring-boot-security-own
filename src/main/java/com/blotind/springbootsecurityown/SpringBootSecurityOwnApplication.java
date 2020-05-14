4b30cd4f9d118237547a4c0a0c73280269c169d8
Brendan Truter <blotind@gmail.com>
2020-05-14 11:38:23 +0200

    init

---
+++ src/main/java/com/blotind/springbootsecurityown/SpringBootSecurityOwnApplication.java
@@ -0,0 +1,13 @@
+package com.blotind.springbootsecurityown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOwnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityOwnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blotind/springbootsecurityown/SpringBootSecurityOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blotind.springbootsecurityown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityOwnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
